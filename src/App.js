bcd8b21f302f7ee483c7af47c37213683ce65b2d
shaun <shaunhegarty@gmail.com>
2019-12-17 23:18:32 +0000

    Set up basic skeleton components and style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Letter extends React.Component {
+  render() {
+    return (<td className="letter">{this.props.letter}</td>)
+  }
+}
+
+class LettersDisplay extends React.Component {
+  render () {
+    const lettersArray = this.props.letters.toUpperCase().split('');
+    const letters = lettersArray.map(function(character) {
+      return (<Letter letter={character}/>);
+    });
+    return (
+      <table id="letters-display">
+        <tbody>
+          <tr>
+            {letters}
+          </tr>
+        </tbody>              
+      </table>)
+  }
+}
+
+class LettersGame extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {mix: 'ajshdljsd'};
+  }
+  render () {
+    return (<div className="letters-game">
+          <p> The game goes here. </p>
+          <LettersDisplay letters={this.state.mix}/>
+        </div>
+      )
+  }
+}
+
+function App () {
+  return (<LettersGame/>);
 }
 
 export default App;
