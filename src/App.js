f22580db268dc16641b35ad004830971ff7e4dc0
shaun <shaunhegarty@gmail.com>
2019-12-18 03:29:49 +0000

    Added most core ui components
    Add placeholder for gametimer.
    Added placeholder button for beginning a round
    Added a button to clear the current letters selection
    Added a word entry that can save words

--- src/App.js
+++ src/App.js
@@ -19,9 +19,26 @@ function strOfParticularLength(string, length) {
     return string;
   }
 }
+
+class GameTimer extends React.Component {
+  render () {
+    return (<div id="game-timer">Game Timer</div>)
+  }
+}
+
+class RoundController extends React.Component {
+  render () {
+    return (<div id="round-controller">
+      <GameTimer />
+      <button id="round-begin">Begin Round</button>
+      <button id="clear-letters" onClick={this.props.clearHandler}>Clear Letters</button>
+    </div>)
+  }
+}
+
 class LettersDisplay extends React.Component {
   render() {
-    const mix = strOfParticularLength(this.props.letters, 9);
+    const mix = strOfParticularLength(this.props.letters, this.props.gameSize);
     const lettersArray = mix.toUpperCase().split('');
     const letters = lettersArray.map(function (character, index) {
       return (<Letter key={index} letter={character} />);
@@ -65,15 +82,15 @@ class ConsonantVowelSelection extends React.Component {
   }
 
   randomLetterFromList(letterList) {
-    return Math.floor(Math.random() * letterList.length)    
+    return Math.floor(Math.random() * letterList.length)
   }
 
   popVowel() {
     let vowelList = this.state.vowelList.slice()
     const index = this.randomLetterFromList(vowelList);
     const letter = vowelList[index];
-    vowelList = vowelList.slice(0, index) + vowelList.slice(index + 1);   
-    this.setState({vowelList: vowelList});
+    vowelList = vowelList.slice(0, index) + vowelList.slice(index + 1);
+    this.setState({ vowelList: vowelList });
     return letter
   }
 
@@ -81,8 +98,8 @@ class ConsonantVowelSelection extends React.Component {
     let consonantList = this.state.consonantList.slice()
     const index = this.randomLetterFromList(consonantList);
     const letter = consonantList[index];
-    consonantList = consonantList.slice(0, index) + consonantList.slice(index + 1);   
-    this.setState({consonantList: consonantList});
+    consonantList = consonantList.slice(0, index) + consonantList.slice(index + 1);
+    this.setState({ consonantList: consonantList });
     return letter
   }
 
@@ -104,26 +121,79 @@ class ConsonantVowelSelection extends React.Component {
   }
 }
 
+class WordEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSave = this.handleSave.bind(this);
+  };
+
+  handleSave() {
+    console.log('Attempting to save word')
+    const input = document.getElementById('word-entry')
+    this.props.saveHandler(input.value);
+    input.value = "";
+  }
+
+  render() {
+    return (<div id="word-entry-div">
+      <input type="text" id="word-entry" maxlength={this.props.maxlen}/>
+      <button id="save-word" onClick={this.handleSave}>Save Word</button>
+    </div>)
+  }
+}
+
+class SavedWords extends React.Component {
+  render() {
+    const savedWords = this.props.savedWords.map(function(word){
+      return <td key={word}>{word}</td>
+    });
+    return (<table id="saved-word-table">
+      <tbody>
+        <tr>
+          {savedWords}
+        </tr>
+      </tbody>
+    </table>)
+  }
+}
 
 class LettersGame extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { mix: '', gameSize: 9 };
+    this.state = { mix: '', 
+    gameSize: 9,
+    savedWords: [] };
 
     // This will not work inside the function when it is passed, if this is not bound
-    this.handleNewLetter= this.handleNewLetter.bind(this);
+    this.handleNewLetter = this.handleNewLetter.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+    this.handleSaveWord = this.handleSaveWord.bind(this);
   };
 
   handleNewLetter(newLetter) {
-      const currentMix = this.state.mix;
-      this.setState({mix: currentMix + newLetter})
+    const currentMix = this.state.mix;
+    this.setState({ mix: currentMix + newLetter })
+  }
+
+  handleSaveWord(newWord) {
+    let savedWords = this.state.savedWords.slice()
+    savedWords = savedWords.concat(newWord);
+    console.log('Trying to add ' + newWord)
+    this.setState({savedWords: savedWords})
+    console.log(this.state)
+  }
+
+  handleClear() {
+    this.setState({ mix: '', savedWords: []});
   }
 
   render() {
     return (<div className="letters-game">
-      <p> The game goes here. </p>
-      <LettersDisplay letters={this.state.mix} />
-      <ConsonantVowelSelection clickHandler={this.handleNewLetter}/>
+      <RoundController clearHandler={this.handleClear} />
+      <LettersDisplay letters={this.state.mix} size={this.state.gameSize} />
+      <ConsonantVowelSelection clickHandler={this.handleNewLetter} />
+      <WordEntry saveHandler={this.handleSaveWord} maxlen={this.state.gameSize}/>
+      <SavedWords savedWords={this.state.savedWords}/>
     </div>
     )
   }
